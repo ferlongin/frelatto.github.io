1cb6080c0290fcd112060d15c0c51bd2a1f5b7f7
ferlongin <102770258+ferlongin@users.noreply.github.com>
2022-05-29 12:41:48 -0300

    initial commit

---
+++ formulario.js
@@ -0,0 +1,34 @@
+window.onload = paginaCargada;
+
+function paginaCargada()
+{
+    // Evento que captura el envio del formulario
+    var boton = document.getElementById("envioDato");
+    boton.onclick = validarFormulario;
+};
+
+// Función para validar el formulario.
+function validarFormulario()
+{
+    var nombre = document.getElementById("nombre");
+    if(nombre.value == "")
+    {
+      alert("El nombre y apellido son un campo obligatorio");
+    }
+
+    var nombre = document.getElementById("email");
+    if(email.value == "")
+    {
+      alert("El email es un campo obligatorio");
+    }
+
+    var nombre = document.getElementById("telefono");
+    if(telefono.value == "")
+    {
+      alert("El teléfono es un campo obligatorio");
+    }
+
+    return;
+    var formulario = document.getElementById("respuesta");
+    formulario.submit();
+}
